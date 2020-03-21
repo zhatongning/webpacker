efd75a5fc57646fea5d8af434bee704a77a3c2f5
Tony <zhatn98@gmail.com>
2020-03-21 12:48:21 +0800

    guide: assets-manegement

--- guides/readme.md
+++ guides/readme.md
@@ -10,3 +10,28 @@
 - `webpack`对`import`,`export`原生支持，但是你自己的`esnext`的代码需要使用`babel`编译
 - 默认使用`webpack.config.js`作为配置文件，可以使用`--config`指定其他文件
 - 搭配`npm script`使用
+
+### assets management
+
+** webpack 只认识`js(不包括es6)`,`json`，其他的都需要做处理 **
+
+- `css-loader`: 做`css`的处理
+- `style-loader`: 将`css`文件通过`link`插入到`html`中
+- `file-loader`: 处理图片、字体，处理通过`import`导入的图片或者通过`background-image: url(...)`引入的图片，处理字体`@font-face`对字体的加载
+- `html-loader`: 将`html`处理成字符串，也可以处理`<img src="..."/>`对图片的导入。
+- `csv-loader`: 对`csv`文件的处理
+- `xml-loader`: 对`xml`文件的处理
+
+### output-management
+
+- 之前的方式需要将打包生成的`bundle`通过`script`引入到`html`中，如果出现多个输出文件，这种方式不方便管理。`html-webpack-plugin`能够帮助管理引入问题，通过模板或者配置自动生成对应的`html`文件
+- `clean-webpack-plugin`: 清除文件
+- `webpack-manifest-plugin`: `manifest` 记录了`webpack`打包结果
+
+```json
+{
+  "app.js": "app.bundle.js",
+  "print.js": "print.bundle.js",
+  "index.html": "index.html"
+}
+```
---
+++ webpack.config.js
@@ -0,0 +1,42 @@
+const path = require("path")
+const ManifestPlugin = require("webpack-manifest-plugin")
+const HtmlWebpackPlugin = require("html-webpack-plugin")
+const { CleanWebpackPlugin } = require("clean-webpack-plugin")
+
+module.exports = {
+  entry: {
+    app: "./guides/asset_managerment/index.js",
+    print: "./guides/asset_managerment/print.js"
+  },
+  output: {
+    filename: "[name].bundle.js",
+    path: path.resolve(__dirname, "guides/asset_managerment/dist")
+  },
+  mode: "none",
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: [
+          {
+            loader: path.resolve(__dirname, "_css-loader"),
+            options: { msg: "there" }
+          },
+          "style-loader",
+          "css-loader"
+        ]
+      },
+      {
+        test: /\.(png|svg|jpe?g|gif)$/,
+        use: ["file-loader"]
+      }
+    ]
+  },
+  plugins: [
+    new ManifestPlugin(),
+    new CleanWebpackPlugin(),
+    new HtmlWebpackPlugin({
+      title: "Output Management"
+    })
+  ]
+}
